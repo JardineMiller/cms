a1794be2b8296398c1d5da0a6229587291c3fecd
Jardine <jardine.miller@gmail.com>
2019-07-16 10:16:48 +0100

    Put the angular file back into the main solution directory as it was causing so much hassle

---
+++ ApplicationLayer/Commands/CommandResult.cs
@@ -0,0 +1,13 @@
+namespace cms.ApplicationLayer
+{
+    public class CommandResult<T>
+    {
+        public bool Success;
+        public T Response;
+
+        public CommandResult()
+        {
+            Success = false;
+        }
+    }
+}
---
+++ Data Layer/Models/User.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace cms.Data_Layer.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+    }
+}
