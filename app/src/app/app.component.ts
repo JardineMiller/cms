a94f8dc6383af4c68c825882c14893126e0f1e52
Jardine <jardine.miller@gmail.com>
2019-07-17 19:58:55 +0100

    Created a new users component and removed the user information that was previously in the app component over to the user

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,25 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor(private _http: HttpClient) {
-
-  }
-
-  users: any[] = [];
-  userKeys: string[] = [];
-
-  ngOnInit(): void {
-    this._http.get('api/users').subscribe(values => {
-      this.users = values as any[];
-      if (this.users.length) {
-        this.userKeys = Object.keys(this.users[0]);
-      }
-    })
-  }
-}
+export class AppComponent { }
---
+++ app/src/app/users/users.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+  constructor(private _http: HttpClient) {
+
+  }
+
+  users: any[] = [];
+  userKeys: string[] = [];
+
+  ngOnInit(): void {
+    this._http.get('api/users').subscribe(values => {
+      this.users = values as any[];
+      if (this.users.length) {
+        this.userKeys = Object.keys(this.users[0]);
+      }
+    })
+  }
+}
