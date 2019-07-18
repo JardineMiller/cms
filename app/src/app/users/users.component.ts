ab9df0dbcb4595ac78f9876e1edfa4c5c1b5f17c
Jardine <jardine.miller@gmail.com>
2019-07-18 21:42:25 +0100

    Playing around with filtering tables and animating in Angular. It's interesting!

--- app/src/app/users/users.component.ts
+++ app/src/app/users/users.component.ts
@@ -1,27 +1,62 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', animate(200)),
+    ]),
+  ]
 })
 export class UsersComponent implements OnInit {
-  constructor(private _http: HttpClient) {
 
+  constructor(private _http: HttpClient) {
   }
 
+  pageTitle: string = "Users";
   users: any[] = [];
   userKeys: string[] = [];
-  pageTitle: string = "Users";
+  filteredUsers: any[] = [];
+
+  _filterString: string;
+  get filterString(): string {
+    return this._filterString;
+  }
+  set filterString(value: string) {
+    this._filterString = value;
+    this.filteredUsers = this.filterString ? this.filter(this._filterString) : this.users;
+  }
 
   ngOnInit(): void {
     //TODO: This should live in a users service
     this._http.get('api/users').subscribe(values => {
       this.users = values as any[];
+      this.filteredUsers = this.users;
       if (this.users.length) {
         this.userKeys = Object.keys(this.users[0]);
       }
+    })
+  }
+
+  addUser() {
+    let lastId = this.users[this.users.length - 1].id;
+    this.users.push({id: lastId + 1, name: "User", email: "email@provider.com"});
+  }
+
+  delete() {
+    this.users.pop();
+  }
+
+  private filter(filterString: string) {
+    return this.users.filter((each) => {
+      return each.name.indexOf(filterString) > -1 || each.email.indexOf(filterString) > -1;
     })
   }
 }
